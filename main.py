c6cdde70d47c2f2ed3b1ec092fdf877d518c47ab
novice000 <maxwellefemena@gmail.com>
2025-03-26 00:59:12 +0100

    print user

--- main.py
+++ main.py
@@ -5,33 +5,37 @@ import db
 from routers import auth, expense
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
+from dotenv import load_dotenv
+
+load_dotenv()
 
 origins = [
     "http://localhost",
     "http://localhost:3000",
-    "http://localhost:8000",
-    "*"
+    "http://localhost:8000"
 ]
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    db.create_db_and_tables()
+    await db.create_db_and_tables()  # Ensure async
     yield
-    print('gracefully shutting down')
-    db.engine.dispose()
-    
+    print('Gracefully shutting down')
+    await db.engine.dispose()  # Ensure async disposal
+
 app = FastAPI(lifespan=lifespan)
-    
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["Authorization"],
 )
+
 app.include_router(auth.auth_router)
 app.include_router(expense.expense_router)
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8000))  # Use Render's PORT or default to 8000
+    port = int(os.getenv("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
